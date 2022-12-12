0b5ebec943eacffa7b4d7145fb7910e449f2467e
Brian Lambert <brianlambert2408@gmail.com>
2022-12-13 09:39:13 +1100

    Completed tasks

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    validate :check_for_clickbait
+
+
+    def check_for_clickbait
+        titleIncludes = [ 
+            /Won't Believe/i,
+            /Secret/i,
+            /Top \d/i,
+            /Guess/i
+        ]
+
+        if titleIncludes.none? { | ti | ti.match title }
+            errors.add(:title, "Must be clickbait!")
+        end
+    end
 end
